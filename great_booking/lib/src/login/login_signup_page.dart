69e4605b040c45ba0827d55a8a7ce7d7d4f456e8
willimetro <wilfredo.ugalde@gmail.com>
2019-09-11 23:24:52 -0300

    Se sube la fase 2 del login para ingresar al registro de usuarios

---
+++ great_booking/lib/src/login/login_signup_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginSignUpPage extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Great Booking Login'),
+      ),
+      body: Container(
+        child: Text('Hola Great Booking login'),
+      ),
+    );
+  }
+}
