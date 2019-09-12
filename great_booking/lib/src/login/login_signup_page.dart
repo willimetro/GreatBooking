711f1b860254a32c2dc8a62342698108468efc1b
willimetro <wilfredo.ugalde@gmail.com>
2019-09-11 23:49:36 -0300

    Se modifica loginsignuppage de stateless a stateful widget

--- great_booking/lib/src/login/login_signup_page.dart
+++ great_booking/lib/src/login/login_signup_page.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
 
 class LoginSignUpPage extends StatefulWidget {
-  
   @override
   _LoginSignUpPageState createState() => _LoginSignUpPageState();
 }
 
 class _LoginSignUpPageState extends State<LoginSignUpPage> {
+
+  bool _isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Great Booking Login'),
       ),
-      body: Container(
-        child: Text('Hola Great Booking login'),
-      ),
+      body: Stack(
+        children: <Widget>[
+          _showCircularProgress()
+        ],
+      )
     );
+  }
+
+  Widget _showCircularProgress(){
+    if (_isLoading) {
+      return Center(child: CircularProgressIndicator());
+    } 
+    return Container(height: 0.0, width: 0.0,);
   }
 }
